258dfa15fe882105506428565a458b58ca38c613
KikaOrdaz <45240183+KikaOrdaz@users.noreply.github.com>
2025-08-05 15:18:39 +0100

    Initial Commit

---
+++ AllAboutThatCat/AllAboutThatCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutThatCatApp.swift
+//  AllAboutThatCat
+//
+//  Created by Kika Ordaz on 05/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutThatCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllAboutThatCatTests/AllAboutThatCatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutThatCatTests.swift
+//  AllAboutThatCatTests
+//
+//  Created by Kika Ordaz on 05/08/2025.
+//
+
+import Testing
+@testable import AllAboutThatCat
+
+struct AllAboutThatCatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
